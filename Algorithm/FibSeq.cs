4336f56d306943b949f098d3b80f910c320c768f
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-15 02:02:37 +0100

    new kata solved

---
+++ Algorithm/FibSeq.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+    Dictionary<int, long> dict = new Dictionary<int, long>();
+    int n = int.Parse(Console.ReadLine());
+    Console.WriteLine(Fib(n, dict).ToString());
+}
+
+long Fib(int n, Dictionary<int, long> dict)
+{
+    if (dict.TryGetValue(n, out long value))
+        return value;
+    if (n <= 2)
+        return 1;
+    else
+    {
+        dict[n] = Fib(n - 1, dict) + Fib(n - 2, dict);
+        return dict[n];
+    }
+}
