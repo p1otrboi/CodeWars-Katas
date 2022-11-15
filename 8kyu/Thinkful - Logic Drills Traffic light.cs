f64533841c2472635eb14ab4e07d1180cba1f323
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-15 21:45:01 +0100

    new kata solved

---
+++ 8kyu/Thinkful - Logic Drills Traffic light.cs
@@ -0,0 +1,26 @@
+﻿//You 're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+
+//Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+
+//For example, when the input is green, output should be yellow.
+
+namespace CodeWars_Katas._8kyu
+{
+    internal class Thinkful___Logic_Drills_Traffic_light
+    {
+        public static string UpdateLight(string current)
+        {
+            switch (current)
+            {
+                case "green":
+                    return "yellow";
+                case "yellow":
+                    return "red";
+                case "red":
+                    return "green";
+                default:
+                    return "sum ting wong";
+            }
+        }
+    }
+}
