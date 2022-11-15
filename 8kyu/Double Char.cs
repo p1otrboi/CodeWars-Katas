eea25c2bd6f8604af83216e68110668e52d53f6d
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-15 02:03:03 +0100

    new kata solved

--- 8kyu/Double Char.cs
+++ 8kyu/Double Char.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+//Examples(Input->Output):
+//*"String"-> "SSttrriinngg"
+//* "Hello World"-> "HHeelllloo  WWoorrlldd"
+//* "1234!_ "-> "11223344!!__  "
 
 namespace CodeWars_Katas._8kyu
 {
     internal class Double_Char
     {
+        public static string DoubleChar(string s)
+        {
+            string res = "";
+            foreach (char c in s)
+            {
+                res += c;
+                res += c;
+            }
+            return res;
+        }
     }
 }
