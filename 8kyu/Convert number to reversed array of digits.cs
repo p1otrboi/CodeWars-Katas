453c65de3d0c077ca55a2665d8b6b5204f3d661c
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-17 00:40:09 +0100

    new kata solved

---
+++ 8kyu/Convert number to reversed array of digits.cs
@@ -0,0 +1,22 @@
+﻿//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+//Example(Input => Output):
+//35231 => [1,3,2,5,3]
+//0 => [0]
+
+namespace CodeWars_Katas._8kyu
+{
+    internal class Convert_number_to_reversed_array_of_digits
+    {
+        public static long[] Digitize(long n)
+        {
+            var list = new List<long>();
+            foreach (char c in n.ToString())
+            {
+                list.Add(c - 48);
+            }
+            list.Reverse();
+            return list.ToArray();
+        }
+    }
+}
