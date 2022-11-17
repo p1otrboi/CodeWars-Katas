4eba2bb0c8dbc12c4b710d42df473fded34d23c0
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-17 20:46:58 +0100

    new kata solved

---
+++ 7kyu/Disemvowel Trolls.cs
@@ -0,0 +1,19 @@
+﻿//Trolls are attacking your comment section!
+
+//A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+//Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+//For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+//Note: for this kata y isn't considered a vowel.
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars_Katas._7kyu
+{
+    internal class Disemvowel_Trolls
+    {
+        public static string Disemvowel(string str) => Regex.Replace(str, "[aeiouAEIOU]", string.Empty);
+    }
+}
