9f198de47be326c0f60534a43a3bfc8c4b7446f6
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-17 21:32:43 +0100

    new kata solved

---
+++ 7kyu/Exes and Ohs.cs
@@ -0,0 +1,30 @@
+﻿/*
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and 
+be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+*/
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars_Katas._7kyu
+{
+    internal class Exes_and_Ohs
+    {
+        public static bool XO(string input)
+        {
+            if (!Regex.IsMatch(input.ToUpper(), "[XO]"))
+                return true;
+            if (input.ToUpper().Count(x => x == 'X') == input.ToUpper().Count(x => x == 'O'))
+                return true;
+            else
+                return false;
+        }
+    }
+}
