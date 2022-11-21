5f8ea8dcdb336f5e3f32b1be7bbbf61690980fc2
Joakim Lindgren <joakim.jl.lindgren@gmail.com>
2022-11-21 13:09:53 +0100

    new kata solved

---
+++ 6kyu/Detect Pangram.cs
@@ -0,0 +1,22 @@
+﻿//A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence 
+//    "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+//Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+using System.Text.RegularExpressions;
+
+namespace CodeWars_Katas._6kyu
+{
+    internal class Detect_Pangram
+    {
+        public static bool IsPangram(string str)
+        {
+            var rg = new Regex(@"[^a-z]");
+            var unique = new HashSet<char>(rg.Replace(str.ToLower(), ""));
+            int count = 0;
+            foreach (char c in unique)
+                count++;
+            return count == 26;
+        }
+    }
+}
